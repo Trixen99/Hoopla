389de6a10f6edea75785c907c2b17772e25cb495
Trixen99 <nathanwhitehead99@hotmail.com>
2026-06-28 21:10:14 +0100

    created new start point for weighted-search

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -8,6 +8,10 @@ def main() -> None:
     normalize_parser = subparsers.add_parser("normalize", help="convert a range of numbers into their normalized versions")
     normalize_parser.add_argument("scores", nargs='*', type=float, help="numbers to convert")
 
+    weighted_search_parser = subparsers.add_parser("weighted-search", help="Search both with keyword & semantic")
+    normalize_parser.add_argument("--alpha", nargs='*', default=0.5, type=float, help="constant to dynamically control the weighting between the scores")
+    normalize_parser.add_argument("--limit", nargs='*', default=5, type=float, help="limit")
+
 
     args = parser.parse_args()
 
@@ -15,12 +19,19 @@ def main() -> None:
         case "normalize":
             normalize_numbers(args.scores)
 
+        case "weighted-search":
+            weighted_search_parser(args.alpha, args.limit)
+
+
         case _:
             parser.print_help()
 
 
 def normalize_numbers(scores):
     nom_scores = normalize_scores(scores)
+
+def weightedSearch():
+
 
 
     if nom_scores == None:
